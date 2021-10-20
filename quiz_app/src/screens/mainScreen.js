7a709fa1ad0f5967fe6485ae60cc8ae12ea62db2
Wojciech Nowaczyk <Wojciech.Nowaczyk@carrier.com>
2021-10-20 22:17:37 +0200

    Styled components installation

---
+++ quiz_app/src/screens/mainScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const  MainScreen = () => {
+    return (
+        <MainBox>Main Screen</MainBox>
+    )
+}
+
+const MainBox = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-color: red;
+`
+
+export default MainScreen;

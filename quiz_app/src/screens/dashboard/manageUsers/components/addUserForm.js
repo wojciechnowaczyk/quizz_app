7a7e4242c77160f9ca181de178c954ff93092c13
Wojciech Nowaczyk <Wojciech.Nowaczyk@carrier.com>
2021-11-23 17:45:30 +0100

    Adding user fetch was handled

--- quiz_app/src/screens/dashboard/manageUsers/components/addUserForm.js
+++ quiz_app/src/screens/dashboard/manageUsers/components/addUserForm.js
@@ -5,17 +5,25 @@ import Button from "../../../../components/button";
 
 const AddUserForm = () => {
     const [login, setLogin] = useState('');
-    const [code, setCode] = useState('')
+    const [code, setCode] = useState('');
+    const [usersListToDisplay, setUsersListToDisplay] = useState([])
 
-    const handleSubmit = () => {
-        console.log(login);
-        console.log(code);
+    const handleSaveData = () => {
+        const objectToSend = {
+            date: new Date(),
+            login: login,
+            code: code,
+        }
+        fetch('http://localhost:3002/dashboard/addUser', {headers:{"Content-Type": "application/json", "Access-Control-Allow-Origin" : "*"},method: "POST", mode: "cors", body: JSON.stringify(objectToSend)})
+        .then(response => response.json())
+        .then(res => setUsersListToDisplay(usersListToDisplay.concat([{_id: res.id,...objectToSend}])))
+        .catch(err => console.log("error:" +err))
     }
     return(
         <>
             <InputWithLabel label="Login" id="loginField" onChange={setLogin}/>
             <InputWithLabel label="Code" id="codeField" onChange={setCode}/>
-            <Button title="Save" onPress={handleSubmit} />
+            <Button title="Save" onPress={handleSaveData} />
         </>
     )
 }

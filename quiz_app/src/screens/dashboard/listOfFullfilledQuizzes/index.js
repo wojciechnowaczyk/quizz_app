161f988c5adab1be24e48f6b2dbf44d8c79e154a
Wojciech Nowaczyk <Wojciech.Nowaczyk@carrier.com>
2021-12-07 16:03:13 +0100

    Dynamic linking to user page was implemented

--- quiz_app/src/screens/dashboard/listOfFullfilledQuizzes/index.js
+++ quiz_app/src/screens/dashboard/listOfFullfilledQuizzes/index.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 import palette from '../../../theme/colors';
 import Button from '../../../components/button';
+import { Link } from "react-router-dom";
 
 const ListOfFullfilledQuizzes = () => {
     const [listOfQuizzes, setListOfQuizzes] = useState([])
@@ -15,6 +16,10 @@ const ListOfFullfilledQuizzes = () => {
         fetchListOfQuizzes();
     },[])
 
+    const handleSeeMoreButton = () => {
+
+    }
+
     const displayListOfQuizzes = () => {
         console.log(listOfQuizzes);
         if(listOfQuizzes.length > 0){
@@ -27,7 +32,9 @@ const ListOfFullfilledQuizzes = () => {
                         <Field width={100}>-</Field>
                         <Field width={300}>{quiz.correctAmount}/{quiz.totalAmount}</Field>
                         <Field width={100}>
-                            <Button title="See more" />
+                            <Button>
+                                <Link to={`/dashboard/user/${quiz.userId}`}>See More</Link>
+                            </Button>
                         </Field>
                     </UserRow>
                 )

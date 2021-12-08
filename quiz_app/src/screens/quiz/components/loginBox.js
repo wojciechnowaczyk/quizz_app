247d86d496694d3796a9db5aa508b46e4b617565
Wojciech Nowaczyk <Wojciech.Nowaczyk@carrier.com>
2021-12-08 15:12:38 +0100

    Saving user login cookie was implemented

--- quiz_app/src/screens/quiz/components/loginBox.js
+++ quiz_app/src/screens/quiz/components/loginBox.js
@@ -2,18 +2,26 @@ import React, {useContext, useState} from 'react';
 import InputWithLabel from '../../../components/inputWithLabel';
 import Button from "../../../components/button";
 import { UserContext } from '../../../contexts/userContext';
+import { useCookies } from 'react-cookie';
 
 const LoginBox = () => {
     const [login, setLogin] = useState();
     const [code, setCode] = useState();
-    const user = useContext(UserContext)
+    const [cookies, setCookie] = useCookies();
+    const user = useContext(UserContext);
+    const setUserCookie = (userToken) => {
+        setCookie('userToken', userToken, {path: '/', maxAge: 360})
+    };
     const handleSubmit = () => {
         fetch('http://localhost:3002/userLogin', {headers:{"Content-Type": "application/json", "Access-Control-Allow-Origin" : "*"},method: "POST", mode: "cors", body: JSON.stringify({login: login, code: code, date: new Date()})})
         .then(res => res.json())
         .then(resp => {
             console.log(resp);
-            user.setUserToken(resp.userToken);
-            user.setUserId(resp.userId);
+            if(resp.userToken && resp.userId){
+                user.setUserToken(resp.userToken);
+                user.setUserId(resp.userId);
+                setUserCookie(resp.userToken);
+            }
         })
         .catch(err => console.log(err))
     }

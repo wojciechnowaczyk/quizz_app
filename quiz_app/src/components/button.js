94735b87998045cf2ab29156e4ea5aacaccfd85f
Wojtek Nowaczyk <wojciech.nowaczyk@espo.eu>
2022-03-22 18:52:51 +0100

    Button was redesigned

--- quiz_app/src/components/button.js
+++ quiz_app/src/components/button.js
@@ -1,33 +1,13 @@
-import React from 'react';
-import styled from 'styled-components';
-import palette from '../theme/colors';
+import React from "react";
+import Button from "@mui/material/Button";
 
-const Button = ({title, onPress, styles, children}) => {
-    return(
-        <StyledButton style={styles} onClick={onPress}>
-            {title}
-            {children}
-        </StyledButton>
-    )
-}
+const CustomButton = ({ title, onPress, styles, children }) => {
+  return (
+    <Button variant="contained" onClick={onPress} style={styles}>
+      {title}
+      {children}
+    </Button>
+  );
+};
 
-const StyledButton = styled.button`
-    min-width: 100px;
-    min-height: 50px;
-    display: flex;
-    font-size: 20px;
-    align-items: center;
-    justify-content: center;
-    border: none;
-    font-weight: bold;
-    border-radius: 20px;
-    padding: 15px 20px 15px 20px;
-    cursor: pointer;
-    color: ${palette.white};
-    background-color: ${palette.bondiBlue};
-    &:hover{
-        background-color: ${palette.matisse};
-    }
-`
-
-export default Button;
+export default CustomButton;

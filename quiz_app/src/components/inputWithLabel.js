99a61d81b14bf67ab0146abb851042154ec7ca19
Wojtek Nowaczyk <wojciech.nowaczyk@espo.eu>
2022-04-01 13:48:25 +0200

    Add a new question form was redesigned

--- quiz_app/src/components/inputWithLabel.js
+++ quiz_app/src/components/inputWithLabel.js
@@ -1,32 +1,19 @@
-import React from 'react';
-import styled from 'styled-components';
-import palette from '../theme/colors';
+import React from "react";
+import TextField from "@mui/material/TextField";
 
-const InputWithLabel = ({label, id,onChange, placeholder}) => {
-    return(
-        <>
-            <Label>{label}</Label>
-            <Input  id={id} name={id} onChange={(e) => onChange(e.target.value)} placeholder={placeholder}/>
-        </>
-    )
-}
-
-const Input = styled.input`
-    height: 30px;
-    width: 250px;
-    border-color: transparent;
-    border-bottom: 2px solid ${palette.matisse};
-    padding: 10px;
-    font-family: 'Lato', sans-serif;
-    font-weight: bold;
-    outline: none
-`
-
-const Label = styled.p`
-    font-family: 'Lato', sans-serif;
-    font-size: 20px;
-    font-weight: bold;
-    color: ${palette.white}
-`
+const InputWithLabel = ({ label, id, onChange, placeholder }) => {
+  return (
+    <TextField
+      id={id}
+      name={id}
+      onChange={(e) => onChange(e.target.value)}
+      placeholder={placeholder}
+      label={label}
+      variant="outlined"
+      fullWidth
+      margin="normal"
+    />
+  );
+};
 
 export default InputWithLabel;
